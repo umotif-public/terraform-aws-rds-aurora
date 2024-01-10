57dd75300d7f4abb327f181202b75026a5facd8c
Abdul Wahid <abdul.wahid@umotif.com>
2024-01-10 16:29:33 +0000

    Fix performance insights retention period (#21)
    * fix: retention period for performance insights needs to be null
    
    * chore: update changelog

--- variables.tf
+++ variables.tf
@@ -242,21 +242,21 @@ variable "tags" {
 }
 
 variable "performance_insights_enabled" {
-  description = "Specifies whether Performance Insights is enabled or not."
+  description = "specifies whether performance insights is enabled or not."
   type        = bool
   default     = false
 }
 
 variable "performance_insights_kms_key_id" {
-  description = "The ARN for the KMS key to encrypt Performance Insights data."
+  description = "the arn for the kms key to encrypt performance insights data."
   type        = string
   default     = null
 }
 
 variable "performance_insights_retention_period" {
-  description = "Amount of time in days to retain Performance Insights data. Valid values are 7, 731 (2 years) or a multiple of 31. When specifying performance_insights_retention_period, performance_insights_enabled needs to be set to true."
+  description = "amount of time in days to retain performance insights data. valid values are 7, 731 (2 years) or a multiple of 31. when specifying performance_insights_retention_period, performance_insights_enabled needs to be set to true."
   type        = number
-  default     = 7
+  default     = null
 }
 
 variable "iam_database_authentication_enabled" {
